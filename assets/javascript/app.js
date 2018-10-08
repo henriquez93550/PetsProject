e1ff60c42da818ece9d22c6e332a348023e6244a
Erick Henriquez <henriquez93550@yahoo.com>
2018-10-07 21:04:33 -0500

    reset button added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,9 @@
-
 $("#submit").on("click", function (event) {
     var animalName = $("#animal_name").val().trim()
     var sex = $("#animal_sex").val().trim()
     var zipcode = $("#zipcode").val().trim()
     var age = $("#animal_age").val().trim()
-
-    // var age = $("#animal_age").val().trim()
+    $(this).addClass("btn-danger")  // change the color of the submit button
     event.preventDefault();
     animalName;
 
@@ -22,7 +20,7 @@ $("#submit").on("click", function (event) {
             key: "3f6c4f479ba1c18027badceade662e2c",
             animal: animalName,
             sex: sex,
-            age:age,
+            age: age,
             count: 10,
             'location': zipcode,
             output: 'basic',
@@ -50,18 +48,19 @@ $("#submit").on("click", function (event) {
                 var info1 = $("<div>");
                 var info2 = $("<div>");
                 var info3 = $("<div>");
-                var info5 = $("<div>");
+                var info4 = $("<div>").addClass('item-description');
+                var showMoreButton = $("<a onClick='javascript:void(0)'>").addClass('showMore btn btn-link btn-primary').text('show more >');
                 //var link= $('<a href="response.petfinder.pets.pet[i].description.$t"></a>');
 
                 info1.text("SEX:   " + response.petfinder.pets.pet[i].sex.$t)
                 info2.text("AGE:   " + response.petfinder.pets.pet[i].age.$t)
                 info3.text("CONTACT INFO: EMAIL  " + response.petfinder.pets.pet[i].contact.email.$t)
-              //var  info4=link.text("Click for description ")
+                info4.text("DESCRIPTION:   " + response.petfinder.pets.pet[i].description.$t)
 
-              info5.text("DESCRIPTION:   " + response.petfinder.pets.pet[i].description.$t)
+
 
                 column1.append(image)
-                column2.append(info1, info2, info3, info5)
+                column2.append(info1, info2, info3, info4, showMoreButton)
                 animalDiv.append(column1, column2)
                 $("#name").append(animalDiv);
 
@@ -72,3 +71,27 @@ $("#submit").on("click", function (event) {
 
         })
 })
+
+// reset button to empty out elements to default state
+$("#reset").on("click", function (event) {
+    $(".container input").val("");
+    $(".container option").text(selected);
+    $("#name").empty();
+    event.preventDefault();
+});
+
+$(document).on("click", ".showMore", function (event) {
+
+    event.preventDefault();
+    if ($(this).text() === 'show more >') {
+
+        // hide description
+        $(this).prev('.item-description').addClass('item-description-show-all');
+        $(this).text('< hide');
+    } else {
+        // show description
+        $(this).prev('.item-description').removeClass('item-description-show-all');
+        $(this).text('show more >');
+
+    }
+});
